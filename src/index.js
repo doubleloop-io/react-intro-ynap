e98c09670bcd870cb5b921ee5966e8dce940b66a
Massimo Iacolare <info@massimoiacolare.it>
2020-05-13 15:45:52 +0200

    added bootstrap.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./main.css"
 import React from "react"
 import ReactDOM from "react-dom"
-import "./main.css"
 import App from "./views/app.js"
 
 console.log("Hello World!")
 
-ReactDOM.render(<App name="YNAP" />, document.getElementById("app-container"))
+ReactDOM.render(<App name="World" />, document.getElementById("app-container"))
